72027004f8a89fe43e54cb17413f86cad2a34f89
Alish Vagadiya <alishvagadiya2202@gmail.com>
2024-01-13 22:45:23 +0530

    app and server config separation

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// /src/server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const authRoutes = require('./routes/authRoutes');
+const customerRoutes = require('./routes/customerRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const productRoutes = require('./routes/productRoutes');
+const db = require('./utils/db');
+
+const app = express();
+
+// Apply rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/customers', customerRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/products', productRoutes); // Add this line
+db.sync();
+module.exports = { app };
--- src/server.js
+++ src/server.js
@@ -1,31 +1,6 @@
 // /src/server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
-const authRoutes = require('./routes/authRoutes');
-const customerRoutes = require('./routes/customerRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const productRoutes = require('./routes/productRoutes');
-const db = require('./utils/db');
-
-const app = express();
+const { app } = require('./app');
 const PORT = process.env.PORT || 3000;
-
-// Apply rate limiting middleware
-const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 100, // Limit each IP to 100 requests per windowMs
-});
-
-app.use(limiter);
-
-app.use(bodyParser.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/customers', customerRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/products', productRoutes); // Add this line
-db.sync();
-
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
